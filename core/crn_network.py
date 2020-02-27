Fix None gradient problem, Change default layer number to 3 and decay_root to 8 in extract_non_images

core/crn_network.py
@@ -23,11 +23,11 @@ def refinement_network(features,
     """
     """
     # TODO: necessary for using batch_size??
-    output = {}
     # TODO: check guidance shape. The shape [?,256,256,1] should cause error
     # TODO:remove features variable?
     # TODO:upsample link to output_strides
     # TODO: correct variable scope for feature calling during evaluation
+    output = {}
     with tf.variable_scope(scope, 'refinement_network') as sc:
         end_points_collection = sc.original_name_scope + '_end_points'
         with slim.arg_scope([slim.conv2d],

core/utils.py
@@ -11,7 +11,7 @@ q_sigmoid = lambda x: tf.nn.relu6(x + 3) * 0.16667
 
 
     
-def fc_layer(inputs, 
+def fc_layer(x, 
              layer_size,
              _std,
              reuse=None,
@@ -34,21 +34,17 @@ def fc_layer(inputs,
   Returns:
     Computed features after split separable conv2d.
   """
+    # TODO: tf.layers.dense()?  SEnet-Tensorflow github
     input_size, output_size = layer_size
-    # shape = tf.shape(inputs)
-    # dim = tf.reduce_prod( shape[1:] )
-    # x = tf.reshape(inputs, [-1, dim])
-    # x = tf.layers.flatten(inputs)
-    x = inputs
     with tf.variable_scope(scope, "fc_layer"):
         w = tf.get_variable(
                 "W",
                 initializer=tf.truncated_normal(shape=[input_size, output_size], stddev=_std))
-        # w = w / tf.sqrt(input_size/2)
-        # b = tf.get_variable(
-        #         "b",
-        #         initializer=tf.constant(0.1, shape=[output_size]))
-        output = tf.matmul(x, w, name='fc')
+        w = w / tf.sqrt(input_size/2)
+        b = tf.get_variable(
+                "b",
+                initializer=tf.constant(0.1, shape=[output_size]))
+        output = tf.add(tf.matmul(x, w, name='fc'), b)
     return output
 
 